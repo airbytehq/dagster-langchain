dbe3266c83c91503a4c098b2058b008c6988e8e8
Joe Reuter <joe@airbyte.io>
2023-05-16 10:37:47 +0200

    test with improved loader

--- ingest.py
+++ ingest.py
@@ -48,10 +48,37 @@ airbyte_assets = load_assets_from_airbyte_instance(
     key_prefix="airbyte_asset",
 )
 
+import json
+from typing import List
+
+from langchain.docstore.document import Document
+from langchain.document_loaders.base import BaseLoader
+from langchain.utils import stringify_dict
+
+
+class BetterAirbyteJSONLoader(BaseLoader):
+    """Loader that loads local airbyte json files."""
+
+    def __init__(self, file_path: str):
+        """Initialize with file path. This should start with '/tmp/airbyte_local/'."""
+        self.file_path = file_path
+
+    def load(self) -> List[Document]:
+        """Load file."""
+        text = ""
+        for line in open(self.file_path, "r"):
+            data = json.loads(line)["_airbyte_data"]
+            text += stringify_dict(data)
+            # This helps the LLM to associate attributes with the correct entity
+            # text += "-End of record-"
+        metadata = {"source": self.file_path}
+        return [Document(page_content=text, metadata=metadata)]
+
+
 
 # Airbyte loader
-airbyte_loader = AirbyteJSONLoader(
-    "/tmp/airbyte_local/local_json/products/_airbyte_raw_products.jsonl"
+airbyte_loader = BetterAirbyteJSONLoader(
+    "/tmp/airbyte_local/_airbyte_raw_products.jsonl"
 )
 
 
